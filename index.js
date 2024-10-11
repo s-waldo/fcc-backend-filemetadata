eed5a4bed4cc06d59e8d092b3b1e44f32690c187
s-waldo <stephen.waldo.94@gmail.com>
2024-10-11 09:46:48 -0500

    First Attempt

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/'})
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
